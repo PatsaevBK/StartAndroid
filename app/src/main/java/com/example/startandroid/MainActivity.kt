5b435d38a6f91a0e5da74ac959e8e4b85a245997
Vnuchara <99031976+Vnuchara@users.noreply.github.com>
2023-09-28 20:01:44 +0300

    parent coroutine waits

--- app/src/main/java/com/example/startandroid/MainActivity.kt
+++ app/src/main/java/com/example/startandroid/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.startandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlinx.coroutines.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private var formatter = SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault())
+
+    private val scope = CoroutineScope(Job() + CoroutineName("MyScope"))
+
+    private lateinit var job: Job
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonRun).setOnClickListener {
+            onRun()
+        }
+
+        findViewById<Button>(R.id.buttonCancel).setOnClickListener {
+            onCancel()
+        }
+    }
+
+    private fun onCancel() {
+        log("onCancel")
+        job.cancel()
+    }
+
+    private fun onRun() {
+        scope.launch {
+            log("parent coroutine, start")
+
+            val job = launch {
+                log("child coroutine, start")
+                TimeUnit.MILLISECONDS.sleep(1000)
+                log("child coroutine, end")
+            }
+
+            log("parent coroutine, wait")
+            job.join()
+            log("parent coroutine, end")
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        log("onStop")
+        scope.cancel()
+    }
+
+    private fun log(text: String) {
+        Log.d("TAG", "${formatter.format(Date())} $text [${Thread.currentThread().name}]")
     }
 }

25a9df9661c7ffb220736d525207543e7c179686
Vnuchara <99031976+Vnuchara@users.noreply.github.com>
2023-09-29 15:35:48 +0300

    adding data in context

--- app/src/main/java/com/example/startandroid/MainActivity.kt
+++ app/src/main/java/com/example/startandroid/MainActivity.kt
@@ -8,12 +8,16 @@ import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.coroutines.ContinuationInterceptor
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
 
     private var formatter = SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault())
 
-    private val scope = CoroutineScope(Dispatchers.Default)
+    private val scope = CoroutineScope(Dispatchers.Main)
 
     private lateinit var job: Job
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +44,8 @@ class MainActivity : AppCompatActivity() {
     private fun onRun() {
         scope.launch {
             log("parent coroutine, start")
-
+            val userData = coroutineContext[UserData]
+            Log.d("TAG", userData.toString())
             val data = async { getData() }
             val data2 = async { getData2() }
             val result = "${data.await()}, ${data2.await()}"
@@ -63,6 +68,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onRun2() {
+        log("scope, ${contextToString(scope.coroutineContext)}")
+        scope.launch {
+            log("coroutine, lvl 1, ${contextToString(this.coroutineContext)}")
+            launch(Dispatchers.Default + UserData(1,"Kek", 18)) {
+                log("coroutine, lvl 2, ${contextToString(coroutineContext)}")
+                launch {
+                    log("coroutine, lvl 3 ${contextToString(coroutineContext)}")
+                }
+            }
+        }
     }
 
     override fun onStop() {
@@ -74,4 +89,7 @@ class MainActivity : AppCompatActivity() {
     private fun log(text: String) {
         Log.d("TAG", "${formatter.format(Date())} $text [${Thread.currentThread().name}]")
     }
+
+    private fun contextToString(context: CoroutineContext) =
+        "Job = ${context[Job]}, Dispatcher = ${context[ContinuationInterceptor]}, User = ${context[UserData]}"
 }

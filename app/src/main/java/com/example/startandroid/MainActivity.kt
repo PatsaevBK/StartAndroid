066ff2ace075c66773a6a54648536aa30ccd6fc9
Vnuchara <99031976+Vnuchara@users.noreply.github.com>
2023-09-30 10:53:08 +0300

    every coroutine has its own job. And despatcher inheritenes

--- app/src/main/java/com/example/startandroid/MainActivity.kt
+++ app/src/main/java/com/example/startandroid/MainActivity.kt
@@ -2,10 +2,85 @@ package com.example.startandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlinx.coroutines.*
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+import kotlin.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var formatter = SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault())
+
+    private val scope = CoroutineScope(Dispatchers.Unconfined)
+
+    private lateinit var job: Job
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.buttonRun).setOnClickListener {
+            onRun()
+        }
+
+        findViewById<Button>(R.id.buttonCancel).setOnClickListener {
+            onCancel()
+        }
+        findViewById<Button>(R.id.buttonRun2).setOnClickListener {
+            onRun2()
+        }
     }
+
+    private fun onCancel() {
+        log("onCancel")
+        job.cancel()
+    }
+
+    private fun onRun() {
+        scope.launch {
+            log("start coroutine")
+            val data = getData()
+            log("end coroutine")
+        }
+    }
+
+    private suspend fun getData(): String = suspendCoroutine {
+        log("suspend function, start")
+        thread {
+            log("suspend function, background work")
+            TimeUnit.MILLISECONDS.sleep(1000)
+            it.resume("Data")
+        }
+    }
+    private suspend fun getData2(): String {
+        return ""
+    }
+
+    private fun onRun2() {
+        log("scope, ${contextToString(scope.coroutineContext)}")
+        scope.launch {
+            log("coroutine, lvl 1, ${contextToString(this.coroutineContext)}")
+            launch(Dispatchers.Default ) {
+                log("coroutine, lvl 2, ${contextToString(coroutineContext)}")
+                launch {
+                    log("coroutine, lvl 3 ${contextToString(coroutineContext)}")
+                }
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        log("onStop")
+        scope.cancel()
+    }
+
+    private fun log(text: String) {
+        Log.d("TAG", "${formatter.format(Date())} $text [${Thread.currentThread().name}]")
+    }
+
+    private fun contextToString(context: CoroutineContext) =
+        "Job = ${context[Job]}, Dispatcher = ${context[ContinuationInterceptor]}}"
 }
